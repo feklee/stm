e9b312893fd62fa3d2c14bba0e288e4fd7adc632
Felix E. Klee <felix.klee@inka.de>
2016-04-24 22:42:06 +0200

    Start adding web frontend

---
+++ approach/frontend/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var SerialPort = require("serialport").SerialPort
+
+serialPort.list(function (err, ports) {
+    ports.forEach(function(port) {
+        console.log(port.comName);
+        console.log(port.pnpId);
+        console.log(port.manufacturer);
+    });
+});
