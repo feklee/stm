a4425a52bc5518b60961824d1f4e855e0c376c7e
Felix E. Klee <felix.klee@inka.de>
2016-04-25 17:54:57 +0200

    Fix issues

--- approach/app/index.js
+++ approach/app/index.js
@@ -1,9 +1,11 @@
+/*jslint node: true, maxlen: 80 */
+
 'use strict';
 
-var SerialPort = require("serialport").SerialPort
+var serialPort = require("serialport").SerialPort;
 
-serialPort.list(function (err, ports) {
-    ports.forEach(function(port) {
+serialPort.list(function (ignore, ports) {
+    ports.forEach(function (port) {
         console.log(port.comName);
         console.log(port.pnpId);
         console.log(port.manufacturer);
