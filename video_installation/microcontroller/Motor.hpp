d30d4af6166865bce0b763f0a09903f5d8669bef
Felix E. Klee <felix.klee@inka.de>
2016-06-20 23:00:27 +0200

    Change order of motor pins

--- video_installation/microcontroller/Motor.hpp
+++ video_installation/microcontroller/Motor.hpp
@@ -3,7 +3,7 @@
 #include <Arduino.h>
 
 class Motor {
-  const uint8_t pins_[4] = {2, 3, 4, 5};
+  const uint8_t pins_[4] = {5, 4, 3, 2};
   const uint8_t activationPin_ = 9;
   byte position_ = 0;
   void setPins(byte, byte, byte, byte);
