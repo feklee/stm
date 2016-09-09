6348f93825424a926d454859510a2b8daf8126ac
Felix E. Klee <felix.klee@inka.de>
2016-09-09 10:51:23 +0200

    Add missing utilities

---
+++ new_software/server/public/scripts/util.js
@@ -0,0 +1,15 @@
+/*jslint browser: true, es6: true, maxlen: 80 */
+
+/*global define, window, d3 */
+
+define(function () {
+    "use strict";
+
+    return {
+        // converts to Volts, from integer representation of voltage in units
+        // 0xffff/3.3 V
+        voltFromInteger(x) {
+            return x / (0xffff / 3.3);
+        }
+    };
+});
