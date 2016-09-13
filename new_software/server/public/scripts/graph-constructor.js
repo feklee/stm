38fbec2ac0186d1fbdb3ded96ff024dea7a5b850
Felix E. Klee <felix.klee@inka.de>
2016-09-13 15:42:05 +0200

    Make graphs zoom and panable

--- new_software/server/public/scripts/graph-constructor.js
+++ new_software/server/public/scripts/graph-constructor.js
@@ -6,7 +6,11 @@ define(["util"], function (util) {
     "use strict";
 
     return function (spec) {
-        var positions = [];
+        var positions = [
+            [0, 0, 0, 6649], [0, 0, 0, 13115], [0, 0, 0, -21058],
+            [0, 0, 0, 16754], [0, 0, 0, 15260], [0, 0, 0, 18709],
+            [0, 0, 0, 24235]
+        ]; // todo: reset
         var margin = {
             top: 0, // px
             left: 30, // px
@@ -14,68 +18,83 @@ define(["util"], function (util) {
             right: 50 // px
         };
         var svg = d3.select("body section." + spec.modeName + " svg");
-        var boundingBox = svg.node().getBoundingClientRect();
-        var getMainWidth = function () {
-            return boundingBox.width - margin.left - margin.right;
+        var boundingBox = function () {
+            return svg.node().getBoundingClientRect();
         };
-        var mainHeight = boundingBox.height - margin.top - margin.bottom;
-        var mainWidth = getMainWidth();
-        var axisPadding = 5; // px
+        var mainWidth = function () {
+            return boundingBox().width - margin.left - margin.right;
+        };
+        var mainHeight = function () {
+            return boundingBox().height - margin.top - margin.bottom;
+        };
+        var axisPadding = 1; // px
+        var stretchFactor = 1;
 
         var mainGroup = svg
-            .append("g")
+            .select("g.main")
             .attr("transform",
                     "translate(" + margin.left + "," + margin.top + ")")
             .attr("class", "main");
 
-        var backgroundRect = mainGroup
-            .append("rect")
-            .attr("width", mainWidth)
-            .attr("height", mainHeight);
+        var zoomableGroup = mainGroup.select("g.zoomable");
 
-        var addAxis = function (spec) {
-            var axisName = "axis" + util.ucfirst(spec.orientation);
-            var axis = d3[axisName](spec.scale).ticks(5);
+        var addAxisGroup = function (spec) {
             var group = svg
-                .append("g")
-                .attr("class", spec.class + " axis")
+                .select("g." + spec.class + ".axis")
                 .attr("transform",
                         "translate(" + spec.left + "," + spec.top + ")");
-            group.call(axis);
-            return axis;
+            group.call(spec.axis);
+            return group;
         };
 
         var xScale = d3.scaleLinear()
-            .range([0, mainWidth]);
-        var xAxis = addAxis({
-            scale: xScale,
-            top: margin.top + mainHeight + axisPadding,
+            .range([0, mainWidth()]);
+        var xAxis = d3.axisBottom(xScale).ticks(5);
+        var xAxisGroup = addAxisGroup({
+            "class": "x",
+            top: margin.top + mainHeight() + axisPadding,
             left: margin.left,
-            orientation: "bottom",
-            "class": "x"
+            axis: xAxis
         });
 
         var currentSignalScale = d3.scaleLinear()
             .domain([-0.5, 3.5])
-            .range([mainHeight, 0]);
-        addAxis({
-            scale: currentSignalScale,
+            .range([mainHeight(), 0]);
+        var currentSignalAxis = d3.axisLeft(currentSignalScale).ticks(5);
+        var currentSignalAxisGroup = addAxisGroup({
+            "class": "current-signal",
             top: margin.top,
             left: margin.left - axisPadding - 1,
-            orientation: "left",
-            "class": "current-signal"
+            axis: currentSignalAxis
         });
 
         var zScale = d3.scaleLinear()
             .domain([-0x1000, 0xffff + 0x1000])
-            .range([mainHeight, 0]);
-        addAxis({
-            scale: zScale,
+            .range([mainHeight(), 0]);
+        var zAxis = d3.axisRight(zScale).ticks(5);
+        var zAxisGroup = addAxisGroup({
+            "class": "z",
             top: margin.top,
-            left: margin.left + mainWidth + axisPadding,
-            orientation: "right",
-            "class": "z"
+            left: margin.left + mainWidth() + axisPadding,
+            axis: zAxis
         });
+
+        var transform = d3.zoomIdentity;
+        var zoomed = function () {
+            zoomableGroup.attr("transform", d3.event.transform);
+            transform = d3.event.transform;
+            xAxisGroup.call(xAxis.scale(transform.rescaleX(xScale)));
+            currentSignalAxisGroup.call(
+                currentSignalAxis.scale(d3.event.transform.rescaleY(
+                    currentSignalScale
+                ))
+            );
+            zAxisGroup.call(
+                zAxis.scale(d3.event.transform.rescaleY(zScale))
+            );
+        };
+        var zoom = d3.zoom().on("zoom", zoomed);
+        svg.call(zoom);
 
         var zLine = d3.line()
             .x(function (ignore, i) {
@@ -94,27 +113,40 @@ define(["util"], function (util) {
             });
 
         var render = function () {
-            mainGroup.selectAll("path").remove();
-            mainGroup.append("path")
+            zoomableGroup.selectAll("path").remove();
+            zoomableGroup.append("path")
                 .datum(positions)
                 .attr("class", "z")
                 .attr("d", zLine);
-            mainGroup.append("path")
+            zoomableGroup.append("path")
                 .datum(positions)
                 .attr("class", "current-signal")
                 .attr("d", currentSignalLine);
         };
 
+        var updateClipPath = function () {
+            svg.select("clipPath rect")
+                .attr("width", mainWidth())
+                .attr("height", mainHeight());
+        };
+
         var updateXScale = function () {
-            boundingBox = svg.node().getBoundingClientRect();
-            mainWidth = getMainWidth();
-            backgroundRect.attr("width", mainWidth);
+            updateClipPath();
+
+            var oldWidth = xScale.range()[1];
+            var newWidth = mainWidth();
+            var resizeFactor = newWidth / oldWidth;
+            var oldStretchFactor = spec.maxNoOfPositions / xScale.domain()[1];
+            var stretchFactorChange = stretchFactor / oldStretchFactor;
             xScale
-                .domain([0, spec.maxNoOfPositions - 1])
-                .range([0, mainWidth]);
-            svg.select("g.x.axis").call(xAxis);
-            svg.select("g.z.axis").attr("transform",
-                    "translate(" + (margin.left + mainWidth + axisPadding) +
+                .domain([0, spec.maxNoOfPositions / stretchFactor])
+                .range([0, newWidth]);
+            transform.x = transform.x * resizeFactor * stretchFactorChange;
+            svg.call(zoom.transform, transform);
+            xAxis.scale(transform.rescaleX(xScale));
+            xAxisGroup.call(xAxis);
+            zAxisGroup.attr("transform",
+                    "translate(" + (margin.left + mainWidth() + axisPadding) +
                     "," + margin.top + ")");
             render();
         };
